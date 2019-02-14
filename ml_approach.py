cf849a2c13a002185fbb6098d090d89ce84071eb
sanuradha043 <20288997+sanuradha043@users.noreply.github.com>
2019-02-13 22:06:46 -0600

    First Data Science Commit (#1)

---
+++ ml_approach.py
@@ -0,0 +1,51 @@
+﻿import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+K.set_image_data_format('channels_first')
+
+
+
+def show_images(image_path_original,image_path_check):
+    
+    imageA=mpimg.imread(image_path_original)
+    imageB=mpimg.imread(image_path_check)
+    fig = plt.figure("Comparison")
+ 
+    # show first image
+    ax = fig.add_subplot(1, 2, 1)
+    plt.imshow(imageA, cmap = plt.cm.gray)
+    plt.axis("off")
+ 
+    # show the second image
+    ax = fig.add_subplot(1, 2, 2)
+    plt.imshow(imageB, cmap = plt.cm.gray)
+    plt.axis("off")
+ 
+    # show the images
+    plt.show()
+
+def verify(image_path_original, image_path_check, model): 
+    
+    encoding_original = img_to_encoding(image_path_original,model)
+    encoding_check = img_to_encoding(image_path_check,model)
+  
+    dist = np.linalg.norm(encoding_original-encoding_check)
+    # setup the figure
+    
+    show_images(image_path_original,image_path_check)
+    
+    print ("Distance is " , dist)
+    if dist < 0.7:
+        print("It's similar")
+                
+    else:
+        print("It's not similar")
+    return dist
+
+def verify_ml_approach(image_path_original,image_path_check):
+
+    dist = verify(image_path_original, image_path_check, FRmodel)
+    
+    return dist
+    
---
+++ traditional_approach.py
@@ -0,0 +1,39 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+def mse(imageA, imageB):
+	# the 'Mean Squared Error' between the two images is the
+	# sum of the squared difference between the two images;
+	# NOTE: the two images must have the same dimension
+	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+	err /= float(imageA.shape[0] * imageA.shape[1])
+	
+	# return the MSE, the lower the error, the more "similar"
+	# the two images are
+	return err
+ 
+def compare_images(imageA, imageB, title):
+	# compute the mean squared error and structural similarity
+	# index for the images
+	m = mse(imageA, imageB)
+	#s = ssim(imageA, imageB)
+ 
+	# setup the figure
+	fig = plt.figure(title)
+	plt.suptitle("MSE: %.2f" % (m))
+ 
+	# show first image
+	ax = fig.add_subplot(1, 2, 1)
+	plt.imshow(imageA, cmap = plt.cm.gray)
+	plt.axis("off")
+ 
+	# show the second image
+	ax = fig.add_subplot(1, 2, 2)
+	plt.imshow(imageB, cmap = plt.cm.gray)
+	plt.axis("off")
+ 
+	# show the images
+	plt.show()
